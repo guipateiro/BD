51c0b8fc6176fdc4a2caa2068ded6cd05c690c16
Pedro <pedropasq@ymail.com>
2023-02-14 12:19:52 -0300

    Atualizado com as regrinhas da EQ visão, mas não funcionando corretamente ainda

--- trabalho2/main.cpp
+++ trabalho2/main.cpp
@@ -7,19 +7,6 @@
 #include <algorithm>
 #include <list>
 
-bool compare_data(Data a, Data b){
-	if (a.origem < b.origem)
-		return 1;
-	else if (b.origem < a.origem)
-		return 0;
-	else{
-		if (a.time < b.time)
-			return 1;
-		else
-			return 0;
-	}
-}
-
 int main(){
 	//grafo g = le_grafo();
 	//escreve_grafo(g);
@@ -84,8 +71,9 @@ int main(){
 			// m == fim dos elementos nao processados
 			//std::cout << "teste "<< i << m <<"\n";
 			int n = i;
+			int j;
 			for (; n <= m; n++){
-				for (int j = n + 1; j < m; j++){
+				for (j = n + 1; j < m; j++){
 					if((data[n].dado == data[j].dado) && ((data[n].operacao == 'R' && data[j].operacao == 'W') || (data[n].operacao == 'W' && data[j].operacao == 'W') || (data[n].operacao == 'W' && data[j].operacao == 'R'))){
 						char tempstrint[128];
 						std::sprintf(tempstrint,"%i", data[n].origem);
@@ -99,35 +87,37 @@ int main(){
 				}
 			}
 			if(n_vertices(g) == decompoe(g)){
-//				std::cout <<  "SS";
+//				std::cout <<  "SS ";
 			}
 			else{
-//				std::cout <<  "NS";
+//				std::cout <<  "NS ";
 			}
 
 
-			// coloque seu algoritmo aqui 
-			n = i;
-			std::vector<int> permutations;
-			for (; n <= m; n++){
-				//std::cout << data[n].origem << " ";
-				permutations.push_back(data[n].origem);
-			} 
+			// *** Início de Equivalência por Imagem ***
 
+			// Monta lista de origens de um conflito
+			// Junta lista de dados utilizados
+			std::vector<int> permutations;
+			std::vector<char> data_list;
+			for (n = i; n <= m; n++){
+				permutations.push_back(data[n].origem);
+				data_list.push_back(data[n].dado);
+			} 
 			sort(permutations.begin(), permutations.end());
 			permutations.erase(unique(permutations.begin(), permutations.end()), permutations.end());
-			//std:: cout << "\n";
+			sort(data_list.begin(), data_list.end());
+			data_list.erase(unique(data_list.begin(), data_list.end()), data_list.end());
 
+			// Cria permutações dos dados em copy_data
 			int size = m-i+1;
 			Data copy_data[size];
-
-
+			bool equivalente;
 			do {
-
-
+				equivalente = true;
 				int pos = 0;
 				for (unsigned int perm_index = 0; perm_index < permutations.size(); perm_index++){
-					for (n = i; n < i + size; n++){
+					for (n = i; n <= m; n++){
 						if (data[n].origem == permutations[perm_index]){
 							std::copy(data+n, data+n+1, copy_data+pos);
 							pos++;
@@ -135,28 +125,79 @@ int main(){
 					}
 				}
 
+				// Regra 2
+				for (n = i; n <= m; n++){
+					if (data[n].operacao == 'R'){
+						for (j = n+1; j <= m; j++){
+							if (data[j].operacao == 'W' && data[n].dado == data[j].dado && data[n].origem != data[j].origem){
+								int i_copia = 0;
+								while (copy_data[i_copia].time != data[n].time && copy_data[i_copia].time != data[j].time)
+									i_copia++;
+								if (copy_data[i_copia].time == data[j].time)
+									equivalente = false;
+							}
+							if (!equivalente)
+								break;
+						}
+					}
+					if (!equivalente)
+						break;
+				}
 
+				if (!equivalente)
+					continue;
+				
+				// Regra 3: Fazer lista de variáveis.
+				// Achar o último write de cada variável.
+				// Ver se ele coincide com o último write da S'.
+				
+				for (unsigned int i_ele = 0; i_ele < data_list.size(); i_ele++){
+					for (n = m; n >= i; n--){
+						if (data[n].operacao == 'W' && data[n].dado == data_list[i_ele])
+							break;
+					}
+					if (n == i-1)
+						continue;
+					for (j = size-1; j >= 0; j--){
+						if (copy_data[j].operacao == 'W' && copy_data[j].dado == data[n].dado){
+							if (copy_data[j].time == data[n].time)
+								break;
+							else{
+								equivalente = false;
+								break;
+							}
+						}
+					}
+					if (!equivalente)
+						break;
+				}
+				
 
-				for (int teste = i; teste < i+size; teste++){
+				if (equivalente){
+					/*std::cout << "Time inicial: " << data[i].time << ", Time final: " << data[m].time << "\nO vetor:\n";
+					for (int teste = i; teste <= m; teste++){
+					std::cout << "Data = " << data[teste].time << ", " << data[teste].origem << ", " << data[teste].operacao << ", " << data[teste].dado << "\n";
+				}
+					std::cout << "É equivalente com: \n";
+					for (int teste = 0; teste < size; teste++){
+					std::cout << "Copy = " << copy_data[teste].time << ", " << copy_data[teste].origem << ", " << copy_data[teste].operacao << ", " <<  copy_data[teste].dado << "\n";
+				}*/
+					break;
+				}
+/*
+				for (int teste = i; teste <= m; teste++){
 					std::cout << "Data = " << data[teste].time << ", " << data[teste].origem << ", " << data[teste].operacao << ", " << data[teste].dado << "\n";
 				}
 				for (int teste = 0; teste < size; teste++){
 					std::cout << "Copy = " << copy_data[teste].time << ", " << copy_data[teste].origem << ", " << copy_data[teste].operacao << ", " <<  copy_data[teste].dado << "\n";
 				}
-/*
-				for (int element: permutations)
-					std::cout << element << ' ';
-*/
 				std::cout << "\n";
-
+*/
 			} while (std::next_permutation(permutations.begin(),permutations.end()));
 
-			
-			
+			std::cout << "Equivalência = " << equivalente;
 
-
-
-			// termine seu codigo aqui
+			// *** Fim de Equivalência por Imagem ***
 			
 			std::cout << "\n";
 
